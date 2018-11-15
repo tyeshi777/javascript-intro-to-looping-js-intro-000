7ba9c9492d6fc76a107e52596bed6db202837aa4
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-15 04:38:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,7 +11,7 @@ function forLoop(array) {
 function whileLoop(number) {
   let countdown = number;
   while (countdown > 0) {
-    console.log(--countdown);
+    console.log(countdown--);
   }
   return 'done';
 }
