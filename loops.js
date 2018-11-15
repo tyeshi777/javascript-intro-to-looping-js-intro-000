6e6b9274878702100849e30b5514d567983ca6ba
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-15 04:33:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,10 +17,10 @@ function whileLoop(number) {
 }
 function doWhileLoop(array) {
   function maybeTrue() {
-  return Math.random() >= 0.5 
+  return Math.random() >= 0.5; 
 }
 do {
-  array.pop()
+  array.pop();
 } while {
   
 }
