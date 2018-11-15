261f885f038478a4c7bbaca120f873353603f66f
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-15 04:00:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,8 @@
-for (let i = 0; i < 25; i++) {
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+for (let i = 0; i < arr.length; i++) {
   if (i === 1) {
-    console.log(`I am ${i} strange loop.`);
+    console.log(`I am ${arr[i]} strange loop.`);
   } else {
-    console.log(`I am ${i} strange loops.`);
+    console.log(`I am ${arr[i]} strange loops.`);
   }
 }
